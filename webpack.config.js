687af6e5980685f3b7d99a0d13e598d9ec02b2dc
Robert Haritonov <r@rhr.me>
2019-06-20 19:45:52 +0100

    fixes after merge

--- webpack.config.js
+++ webpack.config.js
@@ -90,16 +90,6 @@ function createConfig(env) {
           exclude: [
             path.resolve(__dirname, 'node_modules'),
           ],
-        },
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: [/node_modules\/(?!(swiper|dom7)\/).*/, /\.test\.jsx?$/],
-
-        },
-        {
-            test: /\.glsl$/,
-            loader: 'webpack-glsl-loader'
         }],
     },
   };
