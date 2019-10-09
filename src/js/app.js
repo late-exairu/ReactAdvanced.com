4f35878c4b8d67675c76412a121a02c0277e8216
Oleg Shelest <lateexairu@gmail.com>
2019-10-09 17:08:33 +0300

    Talks (#39)
    * add lightning talks
    
    * small style fix

--- src/js/app.js
+++ src/js/app.js
@@ -70,7 +70,7 @@ $(function() {
   // Tabs
   $('.js-tabs__tab').on('click', function(e) {
     e.preventDefault();
-    $('.js-tabs__tab, .js-tabs__panel').removeClass('active');
+    $(this).closest('.container').find('.js-tabs__tab, .js-tabs__panel').removeClass('active');
     $(this).add('#' + $(this).attr('id').replace(/\s*tab\s*/, 'panel')).addClass('active');
     $(this).focus();
   });
